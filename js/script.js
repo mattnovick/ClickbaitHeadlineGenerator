4266cd8895d9f090f5ca7762cb2cd6ee194ea0f8
matt novick <mattnovick@outlook.com>
2016-02-02 12:28:58 -0500

    Added FullPage. Created sample generator for lay out.

--- js/script.js
+++ js/script.js
@@ -24,15 +24,53 @@
 // // when select field changes
 // $("#someId").trigger("bananas");
 
+
+
+
+
+
+
+
+
 function GetValue()
 {
-    var myarray= new Array("item1","item2","item3");
+    
     
     var random = myarray[Math.floor(Math.random() * myarray.length)];
 
     var myarray2= new Array("itemA","itemB","itemC");
     var random2 = myarray2[Math.floor(Math.random() * myarray2.length)];
     //alert(random);
-    document.getElementById("message").innerHTML=random + " " + random2;
+
+    var generated= random + " " + random2
+    document.getElementById("message").innerHTML=generated;
 }
+
+
+function GetValue_Lists()
+{
+    var myarray= new Array("27","9","10", "5", "14");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+
+    var myarray2= new Array("Things","Pictures of Fat Guys","Gender-Bending Squirrels", "Freshmen Truths", "Mugshots of Former Reality Stars", "Flashback pics of 90s Sitcom Stars");
+    var random2 = myarray2[Math.floor(Math.random() * myarray2.length)];
+
+    var myarray3= new Array("that will Sexually Awaken You","that will give you the courage to move on","that will restore your faith in humanity", "that will show you how good you've got it", "that will prove human inteligence is a myth", "that will make you open the window and think about jumping out right now");
+    var random3 = myarray3[Math.floor(Math.random() * myarray3.length)];
+
+    var generated= random + " " + random2 + " " + random3
+    document.getElementById("message").innerHTML=generated;
+}
+
+
+
+
+
+
+function setPhrase(phrase) {
+  var url = 'https://twitter.com/intent/tweet?text=' + "I want to put my generated text here" + '&source=webclient';
+  $('#message2').html('<a href="' + url + '">' + "Share!" + '</a>');
+}
+
+// + encode(message)
 
